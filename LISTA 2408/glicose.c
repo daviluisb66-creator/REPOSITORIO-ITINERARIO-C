ece152a7afc87fb9db437e9dd825cf76626298df
daviluisb66-creator <daviluis.b66@gmail.com>
2025-09-25 15:17:53 -0300

    glicose

---
+++ LISTA 2408/glicose.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    double A, B, C;
+    double areaQuadrado, areaTriangulo, areaTrapezio;
+
+    printf("Digite as medidas A, B e C: ");
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    areaQuadrado = A * A;
+    areaTriangulo = (A * B) / 2.0;
+    areaTrapezio = ((A + B) * C) / 2.0;
+
+    printf("AREA QUADRADO = %.4lf\n", areaQuadrado);
+    printf("AREA TRIANGULO = %.4lf\n", areaTriangulo);
+    printf("AREA TRAPEZIO = %.4lf\n", areaTrapezio);
+
+    return 0;
+}
