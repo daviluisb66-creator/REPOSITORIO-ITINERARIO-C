3c57d2a6dd0e21fc59005c18eb551085fcd18b9c
daviluisb66-creator <daviluis.b66@gmail.com>
2025-09-25 15:08:53 -0300

    consumo

---
+++ LISTA 1308/consumo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    double distancia, combustivel, consumo;
+
+    printf("Digite a distancia percorrida (km): ");
+    scanf("%lf", &distancia);
+
+    printf("Digite o total de combustivel gasto (litros): ");
+    scanf("%lf", &combustivel);
+
+    consumo = distancia / combustivel;
+
+    printf("Consumo medio = %.3lf km/l\n", consumo);
+
+    return 0;
+}
