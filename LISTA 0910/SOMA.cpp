6ea99aad668a43c41720feb1d42bc49d77bee3f1
daviluisb66-creator <daviluis.b66@gmail.com>
2025-10-14 18:29:09 -0300

    Create SOMA.cpp

---
+++ LISTA 0910/SOMA.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, soma;
+
+    cout << "Digite o primeiro valor: ";
+    cin >> x;
+    cout << "Digite o segundo valor: ";
+    cin >> y;
+
+    soma = x + y;
+
+    cout << "Soma = " << soma << endl;
+
+    return 0;
+}
